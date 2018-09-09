32a00f520b9196c61a42e0cb3c73b7eb8f9ab355
Mar <marwanest@live.fr>
2018-09-09 10:30:47 -0500

    layout

--- db_1.go
+++ db_1.go
@@ -9,99 +9,89 @@ import (
 
 func main() {
 
-	fmt.Println("\nEnter your Full Command: ")
-
-	reader := bufio.NewReader(os.Stdin)
-	input, _ := reader.ReadString('\n')
-	var commands []string
-	commands = strings.Split(input, " ")
 	// for _, singleCommand := range commands {
 
 	// 	// good idea: use commad[1], command[2], command[3], concat( rest)
 	// }
 
-	var namesOfCities = commands[2:]
-	fmt.Println(namesOfCities)
-	var firstCommand = commands[0]
+	// var namesOfCities = commands[2:]
+	// fmt.Println(namesOfCities)
+
 	for {
+		fmt.Println("\nEnter your Full Command: ")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		var commands []string
+		commands = strings.Split(input, " ")
+		var firstCommand = commands[0]
+		var secondCommand = commands[1]
+		var cc, cn string // city code and city name
+		var startcc, endcc string
+		var connections int
 		if firstCommand == "a" {
-			// Add(ValidateSecondCommand(secondCommand))
-			return
+
+			if secondCommand == "c" {
+				AddC(cc, cn)
+			} else if secondCommand == "a" {
+				AddA()
+			} else if secondCommand == "f" {
+				AddF()
+			} else {
+				fmt.Println("wrong second command, try again")
+			}
+
 		} else if firstCommand == "l" {
-			// list
-			return
+
+			if secondCommand == "c" {
+				LoadC()
+			} else if secondCommand == "a" {
+				LoadA()
+			} else if secondCommand == "f" {
+				LoadF()
+			} else {
+				fmt.Println("wrong second command, try again")
+			}
+
 		} else if firstCommand == "f" {
-			// find
+			Find(startcc, endcc, connections)
 			return
 		} else if firstCommand == "q" {
 			// quit
-			return
+			os.Exit(3)
 		} else {
-			fmt.Println("wrong entry, try again")
-		}
-	}
-
-}
-func ExecuteFirstCommand(c1 string, c2 string) {
-	var firstCommand = c1
-	//var secondCommand = c2
-	for {
-		if firstCommand == "a" {
-			// Add(ValidateSecondCommand(secondCommand))
-			return
-		} else if firstCommand == "l" {
-			// list
-			return
-		} else if firstCommand == "f" {
-			// find
-			return
-		} else if firstCommand == "q" {
-			// quit
-			return
-		} else {
-			fmt.Println("wrong entry, try again")
-		}
-	}
-}
-
-func ValidateSecondCommand(c2 string) string {
-	var secondCommand = c2
-	for {
-		if secondCommand == "c" {
-			return "c"
-		} else if secondCommand == "a" {
-			return "a"
-		} else if secondCommand == "f" {
-			return "f"
-		} else {
-			fmt.Println("wrong entry, try again")
+			fmt.Println("wrong first command, try again")
 		}
 	}
 
 }
 
-func Add(secondCommand string, cc string, cn string) {
-	if secondCommand == "c" {
-		addC(cc, cn)
-	} else if secondCommand == "a" {
-		addA()
-	} else {
-		addF()
-	}
-}
-
-func addC(cc string, cn string) {
+func AddC(cc string, cn string) {
 	var cityCode = cc
 	var cityName = cn
 	fmt.Scan(&cityCode)
 	fmt.Scanln(&cityName)
 }
 
-func addA() {
+func AddA() {
 	fmt.Println("yay made it to a, a")
 
 }
 
-func addF() {
+func AddF() {
+
+}
+
+func LoadA() {
+
+}
+
+func LoadC() {
+
+}
+func LoadF() {
+
+}
+
+func Find(startcc string, endcc string, connections int) {
 
 }
